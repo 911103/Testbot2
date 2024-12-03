e8627eb35d3afbfb174a7af3699a1f2f59b7cc23
911103 <116645136+911103@users.noreply.github.com>
2024-12-03 17:32:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-import re
+
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('eVAHESVfOWguGISa+Ye8fNAWOUGAJ75syoKXrZ3/N8Avz4Dc0pw8hA100mf4xzUXq8SCBb61SUz9/xdYy3UCHev+kXA2QznzDeQf53B0CWDgLb8l4+rMBoLsEsWVGzyp01NUQTJFHAD7l8leF4NrmAdB04t89/1O/w1cDnyilFU=')
+
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('564b63836ccb1084a63387b47d24f43a')
 
@@ -42,20 +43,9 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('告訴我秘密',message):
-        flex_message = TextSendMessage(text='請點選您想去的國家',
-                               quick_reply=QuickReply(items=[
-                                   QuickReplyButton(action=MessageAction(label="日本", text="Japan")),
-                                   QuickReplyButton(action=MessageAction(label="台灣", text="Taiwan")),
-                                   QuickReplyButton(action=MessageAction(label="新加坡", text="Singapore")),
-                                   QuickReplyButton(action=MessageAction(label="韓國", text="Korea")),
-                                   QuickReplyButton(action=MessageAction(label="中國", text="China")),
-                                   QuickReplyButton(action=MessageAction(label="美國", text="US"))
-                               ]))
-        line_bot_api.reply_message(event.reply_token, flex_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+
 #主程式
 import os
 if __name__ == "__main__":
